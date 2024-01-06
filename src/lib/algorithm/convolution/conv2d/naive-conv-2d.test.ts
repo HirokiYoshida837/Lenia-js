eae2a9a15acd25ff46ba1e97ea9a06e6de1cbc95
h.yoshida <hiroki.yoshida01@outlook.jp>
2024-01-06 15:38:47 +0900

    循環畳み込み(2d) を実装

--- src/lib/algorithm/convolution/conv2d/naive-conv-2d.test.ts
+++ src/lib/algorithm/convolution/conv2d/naive-conv-2d.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it} from "vitest";
-import {naiveLinearConv2d} from "@/lib/algorithm/convolution/conv2d/naive-conv-2d";
+import {naiveCyclicConv2d, naiveLinearConv2d} from "@/lib/algorithm/convolution/conv2d/naive-conv-2d";
 
 describe("naive-conv-2d", () => {
 
@@ -101,6 +101,99 @@ describe("naive-conv-2d", () => {
       ]
 
       const ret = naiveLinearConv2d(x, h, n)
+
+      console.log(ret)
+
+      expect(ret).toEqual(expected)
+    })
+  })
+
+  describe('naiveCyclicConv1d', () => {
+    it('循環畳み込みができることのテスト - 1', () => {
+
+      const n = 4;
+      const x = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 0],
+      ]
+
+      const h = [
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]
+
+      const expected = [
+        [0, 0, 0, 0,],
+        [0, 0, 0, 0,],
+        [0, 0, 3, 0,],
+        [0, 0, 0, 0,],
+      ]
+
+      const ret = naiveCyclicConv2d(x, h, n)
+
+      console.log(ret)
+
+      expect(ret).toEqual(expected)
+    })
+
+    it('循環畳み込みができることのテスト - 2', () => {
+
+      const n = 4;
+      const x = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 0],
+      ]
+      const h = [
+        [3, 2, 0, 0],
+        [2, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]
+
+      const expected = [
+        [0, 0, 0, 0,],
+        [0, 0, 0, 0,],
+        [0, 0, 9, 6,],
+        [0, 0, 6, 0,],
+      ]
+
+      const ret = naiveCyclicConv2d(x, h, n)
+
+      console.log(ret)
+
+      expect(ret).toEqual(expected)
+    })
+
+    it('循環畳み込みができることのテスト - 3', () => {
+
+      const n = 4;
+      const x = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 3, 0],
+        [0, 0, 0, 0],
+      ]
+      const h = [
+        [3, 2, 0, 2],
+        [2, 0, 0, 0],
+        [0, 0, 0, 0],
+        [2, 0, 0, 0],
+      ]
+
+      const expected = [
+        [0, 0, 0, 0,],
+        [0, 0, 6, 0,],
+        [0, 6, 9, 6,],
+        [0, 0, 6, 0,],
+      ]
+
+      const ret = naiveCyclicConv2d(x, h, n)
 
       console.log(ret)
 
--- src/lib/algorithm/convolution/conv2d/naive-conv-2d.ts
+++ src/lib/algorithm/convolution/conv2d/naive-conv-2d.ts
@@ -2,13 +2,13 @@ import Enumerable from "linq";
 
 /**
  * 周期 n の離散信号(2次元)同士の線形畳み込み[Linear Convolution]を行います。
- * ナイーブに処理を行うため、計算量は `O(n^2)`となっています。
+ * ナイーブに処理を行うため、計算量は `O(n^4)`となっています。
  * <br/>
  * 動作イメージはこちらを参考にしてください : [直線畳み込みと循環畳み込み](https://vrlab.meijo-u.ac.jp/edu/linear-and-cyclic-convolution.html) <br/>
  * refs : [線形および循環畳み込み - MATLAB & Simulink - MathWorks 日本](https://jp.mathworks.com/help/signal/ug/linear-and-circular-convolution_ja_JP.html)
  * @param f 元信号
  * @param g カーネル信号
- * @param n 要素数 (2べき)
+ * @param n 1辺の要素数 (2べき)
  */
 export const naiveLinearConv2d = (f: number[][], g: number[][], n: number): number[][] => {
 
@@ -52,7 +52,64 @@ export const naiveLinearConv2d = (f: number[][], g: number[][], n: number): numb
     }
   }
 
-  console.log(ret)
+  // console.log(ret)
+
+  return ret;
+}
+
+/**
+ * 周期 n の離散信号(2次元)同士の線形畳み込み[Cyclic Convolution]を行います。
+ * ナイーブに処理を行うため、計算量は `O(n^4)`となっています。
+ * <br/>
+ * 動作イメージはこちらを参考にしてください : [直線畳み込みと循環畳み込み](https://vrlab.meijo-u.ac.jp/edu/linear-and-cyclic-convolution.html) <br/>
+ * refs : [線形および循環畳み込み - MATLAB & Simulink - MathWorks 日本](https://jp.mathworks.com/help/signal/ug/linear-and-circular-convolution_ja_JP.html)
+ * @param f 元信号
+ * @param g カーネル信号
+ * @param n 1辺の要素数 (2べき)
+ */
+export const naiveCyclicConv2d = (f: number[][], g: number[][], n: number): number[][] => {
+
+  if (f.length != n) {
+    throw new Error('Illegal argument. f.length is not equal n')
+  }
+
+  if (!Enumerable.from(f).select(x => x.length == n).all(x => x)) {
+    throw new Error('Illegal argument. f is not n square matrix')
+  }
+
+  if (g.length != n) {
+    throw new Error('Illegal argument. f.length is not equal n')
+  }
+
+  if (!Enumerable.from(g).select(x => x.length == n).all(x => x)) {
+    throw new Error('Illegal argument. f is not n square matrix')
+  }
+
+  const ret = Enumerable.range(0, n)
+    .select(x => Enumerable.range(0, n).select(y => 0).toArray())
+    .toArray();
+
+  // ナイーブに畳み込み処理を計算する。計算量は O(n^4) となる。
+  for (let fi = 0; fi < n; fi++) {
+    for (let fj = 0; fj < n; fj++) {
+
+      const fij = f[fi][fj]
+
+      for (let gi = 0; gi < n; gi++) {
+        for (let gj = 0; gj < n; gj++) {
+
+          // console.log(`gi:${gi}, gj:${gj}, g[gi][gj]:${g[gi][gj]}`)
+          // console.log(`fij: ${fij}`)
+          const v = fij * g[gi][gj]
+          // console.log(`v: ${v}`)
+
+          ret[(fi + gi) % n][(fj + gj) % n] += fij * g[gi][gj]
+        }
+      }
+    }
+  }
+
+  // console.log(ret)
 
   return ret;
 }
